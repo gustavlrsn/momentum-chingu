ebbd731568539726b613152c0dc1613da36e89b9
Gustav Larsson <gustav.larsson@gmail.com>
2017-03-28 17:00:36 +0200

    Add a time output

--- newTab.js
+++ newTab.js
@@ -1,6 +1,6 @@
 var goal = '';
 
-function init() {
+function initGoal() {
   var goalInput = document.querySelector('#goalinput');
   //var goalText = document.querySelector('#goaltext');
 
@@ -19,4 +19,20 @@ function init() {
   });
 }
 
-init();
+function displayWatch() {
+  var timeElement = document.querySelector('#time');
+  setInterval(function() {
+    var d = new Date();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var time = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
+    timeElement.innerHTML = time;
+  }, 1000);
+}
+
+function displayWeather() {
+  
+}
+
+displayWatch();
+initGoal();
